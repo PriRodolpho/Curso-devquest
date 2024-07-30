dda763797652049dc410e01ad4c873834a6858e2
PriRodolpho <ogrouverde@gmail.com>
2024-07-30 17:56:04 -0300

    Upload: Variáveis

---
+++ modulo-javascript-basico/03-variaveis/index.js
@@ -0,0 +1,12 @@
+// Pouco usada por ser global, ela permite que a variável seja sobrescrita
+var nomePessoa = "Caetano";
+
+// Tipo local,não aceita o valor declarado
+const emailPessoa = "caetano@gmail.com"
+
+// Tipo local, permite mudança, mas de acordo com o local
+let telefonePessoa = "95234-5654"
+
+telefonePessoa = "11111-4111111"
+
+console.log(telefonePessoa)
