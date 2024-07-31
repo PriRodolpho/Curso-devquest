26326cacef28632a5175bb9700f6cd44cef1c5d2
PriRodolpho <ogrouverde@gmail.com>
2024-07-31 13:05:42 -0300

    Upload: tipos primitivos

---
+++ modulo-javascript-basico/04-tipos-primitivos/index.js
@@ -0,0 +1,18 @@
+// string
+let nome = "Pri";
+
+// number
+let numero = 42;
+
+// boolean
+let usuarioVerificado = false;
+
+if(usuarioVerificado){
+    console.log("usuário verificado")
+}else{
+    console.log("usuário não verificado")
+}
+
+// undefined
+let telefone = undefined;
+console.log(telefone)
