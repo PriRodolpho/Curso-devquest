3d8820a8e81ea21964081f7835ab4b4f297f1b6b
PriRodolpho <ogrouverde@gmail.com>
2024-08-05 10:04:59 -0300

    Upload: operadores lógicos

---
+++ modulo-javascript-basico/05-operadores-lógicos/index.js
@@ -0,0 +1,15 @@
+let primeiroValorLogica = 1
+let segundoValorLogica = 5
+
+console.log(primeiroValorLogica == segundoValorLogica)
+console.log(primeiroValorLogica === segundoValorLogica)
+console.log(primeiroValorLogica !== segundoValorLogica)
+console.log(primeiroValorLogica != segundoValorLogica)
+console.log(primeiroValorLogica >= segundoValorLogica)
+console.log(primeiroValorLogica <= segundoValorLogica)
+console.log(primeiroValorLogica > segundoValorLogica)
+console.log(primeiroValorLogica < segundoValorLogica)
+
+console.log(primeiroValorLogica == segundoValorLogica || primeiroValorLogica != segundoValorLogica)
+console.log(primeiroValorLogica == segundoValorLogica && primeiroValorLogica != segundoValorLogica)
+console.log(!(primeiroValorLogica == segundoValorLogica))
