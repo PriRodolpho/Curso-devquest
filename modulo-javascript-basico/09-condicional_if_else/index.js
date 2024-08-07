1883a3cb3e681b9f6a807dbf2c81f19bd981accf
PriRodolpho <ogrouverde@gmail.com>
2024-08-07 18:33:14 -0300

    Upload: condicionais if else

---
+++ modulo-javascript-basico/09-condicional_if_else/index.js
@@ -0,0 +1,15 @@
+let corGato = 'Laranja'
+
+if(corGato === 'Preto'){
+    console.log('É a Morgana')
+}else if(corGato == 'Laranja'){
+    console.log('É Ísis')
+}else if(corGato === 'Branco com cinza'){
+    console.log('É Al Pacino')
+}else{
+    console.log('É Rolinha')
+}
+
+let previsaoTempo = 'Ensolarado'
+
+previsaoTempo === 'Chuvoso' ? console.log('Leve um guarda-chuva') : console.log('Deixe o guarda-chuva em casa')
