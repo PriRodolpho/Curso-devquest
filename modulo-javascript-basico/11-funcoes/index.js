9101f1657fbb2806e8bfc19c6ecfc3f29b44119f
PriRodolpho <ogrouverde@gmail.com>
2024-08-09 13:21:06 -0300

    Upload: function

---
+++ modulo-javascript-basico/11-funcoes/index.js
@@ -0,0 +1,13 @@
+function listaSacolao(fruta1 = "banana", fruta2 = "maçã"){
+    return "Preciso comprar " + fruta1 + " e " + fruta2
+}
+
+let compras = listaSacolao("uva", "lichia")
+
+console.log(compras)
+
+function devoTomarCha(){
+    console.log("Sempre é tempo para chá")
+}
+
+devoTomarCha()
