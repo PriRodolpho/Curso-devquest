a7bc06242b740d81a7720d7908d975ba45960c21
PriRodolpho <ogrouverde@gmail.com>
2024-08-11 08:49:56 -0300

    Upload: Exercício básico js 3

---
+++ modulo-javascript-basico/14-exercicio-basico-js-3/index.js
@@ -0,0 +1,7 @@
+let decisaoQuester = confirm("Seja bem vindo(a), aventureiro(a)! Gostaria de pagar R$ 50,00 para passar a noite na nossa hospedagem?")
+
+if(decisaoQuester === true){
+    console.log("Ótimo! Nós temos as melhores camas de toda a região!")
+}else{
+    console.log("Que pena! Você parecia ser uma pessoa mais legal")
+}
