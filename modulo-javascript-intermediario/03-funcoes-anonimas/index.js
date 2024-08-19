6c237c2d461c69b6bca9f757d7bd023acec2e337
PriRodolpho <ogrouverde@gmail.com>
2024-08-19 13:09:32 -0300

    Upload: funções anônimas

---
+++ modulo-javascript-intermediario/03-funcoes-anonimas/index.js
@@ -0,0 +1,14 @@
+// // Uma função simples
+// function somar(numero1, numero2){
+//     return numero1 + numero2
+// }
+
+// let resultadoDaSoma = somar(23, 42)
+// console.log(resultadoDaSoma);
+
+// uso da função anônima
+let somar = function(numero1, numero2){
+    return numero1 + numero2
+}
+
+console.log(somar(23, 42));
