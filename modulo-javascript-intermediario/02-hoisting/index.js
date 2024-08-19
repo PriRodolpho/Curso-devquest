096f344602e12c43ac14ae4d13549324a1277fcc
PriRodolpho <ogrouverde@gmail.com>
2024-08-19 12:48:00 -0300

    Upload: hoisting

---
+++ modulo-javascript-intermediario/02-hoisting/index.js
@@ -0,0 +1,15 @@
+// // Içada e inicializada
+// console.log(pais)
+// var pais
+// pais = "Brasil"
+
+// // Içada e não inicializada
+
+// console.log(pais)
+// let pais
+// pais = "Brasil"
+
+// Içada e inicializada da forma certa de acordo com esse tipo de variável
+let pais
+console.log(pais)
+pais = "Brasil"
